1e9ff9c93db6d613edcf2a650b8e8232cb20e191
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-10-31 09:35:48 +0700

    integer

--- index.php
+++ index.php
@@ -1,37 +1,40 @@
 <?php
 
-// BOOLEAN
-/*
- integers: 0 & -0 => false
- floats: 0.0 & -0.0 => false
- string: '' & '0' => false
- array: [] => false
- null: false
+// INTEGER
+/* 
+nilai max dari integer tergantung dari platformnya: 32 bit beda dengan 64 bit
+kita bisa cek denan PHP_INT_MIN dan PHP_INT_MAX
 */
 
-/*
-anything else will be true:
-'5', 5, [5], 'false'
-*/
+echo PHP_INT_MAX . "<br>\n";
+echo PHP_INT_MIN . "<br>\n";
 
-$isComplete = true;
-$isComplete2 = (string) true;
-var_dump($isComplete); //bool(true)
-var_dump($isComplete2); //string(1) "1"
+// integer can be represented in: 
+// decimal or base 10:
+$x = 5;
 
-if($isComplete){
-  echo 'True';
-} else {
-  echo 'False';
-}
+// hexadecimal or base 16 - diawali 0x:
+$y = 0x2A;
 
-echo $isComplete;
-/*
-if true, akan print 1
-if false, eakan print empty string
-*/
+// octal or base 8 - diawali 0:
+$z = 055;
 
-echo (string) $isComplete;
+// binary or base 2 - diawali 0b:
+$b = 0b110;
 
-echo (is_bool($isComplete));
-var_dump (is_bool($isComplete));
+echo $x . "<br>\n";
+echo $y . "<br>\n";
+echo $z . "<br>\n";
+echo $b . "<br>\n";
+
+// ketika kita membuat bilangan yang melebihi MAX integer, maka dia akan berubah menjadi float
+$max = PHP_INT_MAX + 1; //ini akan jadi float
+
+// cara mengubah suatu variable ke integer adalah dengan menggunakan casting:
+$cast = (int) true;
+echo $cast . "<br>\n";
+
+
+// kita bisa gunakan _ untuk readibility
+$mill = 200_000_000;
+echo $mill;
