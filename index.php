21a2bf9eb5bc96291dd28896e184b1ce4b8578fc
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-11-01 22:37:04 +0700

    switch

--- index.php
+++ index.php
@@ -1,90 +1,29 @@
 <?php
 
-// LOOPS
+// SWITCH
 
-// while
+// This switch statement:
+
 $i = 1;
-while ($i <= 10) {
-    echo $i++;  /* the printed value would be
-                   $i before the increment
-                   (post-increment) */
-}
-echo '<br>';
 
-// while with break
-$j = 1;
-while (true) {
-  if($j > 10){
-    break;
-  }
-  echo $j++;
-}
-echo '<br>';
-
-// multiple while with break
-$k = 1;
-while (true) {
-  while ($k > 10){
-    break 2; // break 2 level of while loops
-  }
-  echo $k++;
-}
-echo '<br>';
-
-// while with continue
-$a = 1;
-while ($a < 10){
-  if($a % 2 == 0){
-    $a++;
-    continue;
-  }
-  echo $a++;
-}
-
-echo '<br>';
-
-// do-while
-$i = 1;
-do {
-    echo $i;
-    $i++;
-} while ($i <= 10);
-echo '<br>';
-
-// for
-// for model 1
-for ($i = 1; $i <= 10; $i++) {
-  echo $i;
-}
-echo '<br>';
-
-// for model 2
-for ($i = 1; ; $i++) {
-  if ($i > 10) {
+switch ($i) {
+  case 0:
+      echo "i equals 0";
       break;
-  }
-  echo $i;
-}
-echo '<br>';
-
-// for model 3
-$i = 1;
-for (; ; ) {
-  if ($i > 10) {
+  case 1:
+      echo "i equals 1";
       break;
-  }
-  echo $i;
-  $i++;
+  case 2:
+      echo "i equals 2";
+      break;
 }
-echo '<br>';
 
-// for model 4
-for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
-echo '<br>';
+// Is equivalent to:
 
-
-// foreach - for array
-$arr = array(1, 2, 3, 4);
-foreach ($arr as &$value) {
-    $value = $value * 2;
+if ($i == 0) {
+    echo "i equals 0";
+} elseif ($i == 1) {
+    echo "i equals 1";
+} elseif ($i == 2) {
+    echo "i equals 2";
 }
